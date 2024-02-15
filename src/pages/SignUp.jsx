d16e05cdcd434b62355f6079c28cee2f7c998455
JohnLorgensky <johnantilus@gmail.com>
2024-02-14 20:41:10 -0500

    create private route and custom hook for protecting the profile page and add the logout functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -57,7 +57,7 @@ export default function SignUp() {
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto '>
-        <div className='md:w-[67% lg:w-[50%] mb-12 md:mb-6'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2V5fGVufDB8fDB8fHww" alt='key-image' className="w-full rounded-2xl" />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
